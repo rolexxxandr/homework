6a2c5e3f33a53bc0325ccd610cbc92ca28da5117
rolexxxandr <119816886+rolexxxandr@users.noreply.github.com>
2022-12-09 01:58:13 +0200

    Update main.cpp

--- lab4_v16/main.cpp
+++ lab4_v16/main.cpp
@@ -5,43 +5,55 @@
 
 int calculation(int str[ELEMENTS])
 {
-    int elements[ELEMENTS];
-    int count[ELEMENTS];
     int counter = 1;
+    int highest = 0;
     
-    elements[0] = str[0];
-
     for (int i = 1; i < ELEMENTS; i++)
     {
         if (str[i] == str[i - 1])
             counter++;
         else
         {
-            count[i - 1] = counter;
+            if (counter > highest)
+                highest = counter;
+            
             counter = 1;
-            elements[i] = str[i];
         }
+        if (counter > highest)
+            highest = counter;
     }
-    count[ELEMENTS - 1] = counter;
-
-    for (int i = 0; i < ELEMENTS; i++)
-    {
-        printf("%d - %d\n", elements[i], count[i]);
-    }
-    
-    return 0;
+    return highest;
 }
 
 int main()
 {
     int matrix[STRINGS][ELEMENTS] = {
-        {0, 1, 2, 2, 1, 3, 3, 3},
-        {0, 7, 2, 4, 5, 6, 9, 1}, 
-        {0, 7, 2, 4, 5, 6, 9, 1}, 
-        {0, 7, 2, 4, 5, 6, 9, 1}, 
+        {0, 1, 2, 2, 3, 1, 3, 3},
+        {0, 3, 3, 3, 2, 2, 2, 2}, 
+        {0, 5, 5, 5, 5, 5, 9, 1}, 
+        {0, 0, 0, 0, 0, 0, 0, 0}, 
         {0, 7, 2, 4, 5, 6, 9, 1} };
 
     calculation(matrix[0]);
 
     int total_count[STRINGS];
+
+    for (int i = 0; i < STRINGS; i++)
+    {
+        total_count[i] = calculation(matrix[i]);
+    }
+
+    for (int j = 0; j < STRINGS; j++)
+    {
+        printf("string N%d strike : %d\n", (j + 1), total_count[j]);
+    }
+
+    int index = 0;
+    for (int k = 1; k < STRINGS; k++) 
+    {
+        if (total_count[k] > total_count[k - 1])
+            index = k;
+    }
+    printf("the highest strike has string N%d: %d\n", (index + 1), total_count[index]);
+    
 }
